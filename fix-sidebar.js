464c27d7d400ed6203bce9392e9d76686ad44cd6
T.KITTIPONG <kitipongzaza566@gmail.com>
2026-08-30 18:58:30 +0700

    chore: remove temp script

--- fix-sidebar.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let css = fs.readFileSync('css/style.css', 'utf8');
-
-css = css.replace(/\.sidebar-links \{/, '.sidebar-links {\n    flex: 1;\n    justify-content: flex-start;');
-
-fs.writeFileSync('css/style.css', css, 'utf8');
-console.log("Added flex: 1 to sidebar-links");
